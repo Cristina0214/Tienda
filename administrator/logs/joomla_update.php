f96fa550cfe0c3fdb3081e44c197096218f268c1
Cristina0214 <1730370@upv.edu.mx>
2020-02-04 01:47:11 +0000

    tienda agregada

---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-02-04 01:01:16 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-02-04T01:01:16+00:00	INFO 187.136.2.220	update	COM_JOOMLAUPDATE_UPDATE_LOG_DELETE_FILES
